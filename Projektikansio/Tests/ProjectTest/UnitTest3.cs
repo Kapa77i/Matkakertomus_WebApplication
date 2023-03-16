30f8493b9f4b33bc3edaac0901ed626e2d6cd23b
Simo Hämäläinen <76773814+Hovari@users.noreply.github.com>
2023-03-16 18:03:25 +0200

    unitTests

--- Projektikansio/Tests/ProjectTest/UnitTest3.cs
+++ Projektikansio/Tests/ProjectTest/UnitTest3.cs
@@ -1,1 +1,44 @@
-﻿
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using Assert = Xunit.Assert; // Tarkentaa, että käytetään XUnit Assertia
+
+namespace ProjectTest
+{
+    [TestClass]
+    public class FuckYouTest : TestContext
+    {
+        public override IDictionary Properties => throw new NotImplementedException();
+
+        public override void AddResultFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void TestMethod1()
+        {
+            bool result = true;
+            Assert.True(result);
+        }
+
+        public override void Write(string? message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(string format, params object?[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteLine(string? message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteLine(string format, params object?[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

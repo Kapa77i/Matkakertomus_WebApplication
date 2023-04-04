f9304abf2919bb96b445dc35fb7e4631b99aca42
Katja Papinniemi <Katja.Papinniemi@edu.savonia.fi>
2023-04-04 18:46:47 +0300

    Porukan matkat unit testiä ajateltu. Ei toteutettu.

--- Projektikansio/Tests/ProjectTest/UnitTest4.cs
+++ Projektikansio/Tests/ProjectTest/UnitTest4.cs
@@ -1,9 +1,40 @@
 ﻿using Bunit;
+using FrontEnd.Pages;
+using FrontEnd.Shared;
+using Microsoft.Extensions.DependencyInjection;
+using SharedLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ProjectTest
 {
-    public class GroupTripsTest : TestContext
+    public class GroupTrips : TestContext
     {
+        [Fact]
+        public async Task TestGrouptripsPage()
+        {
+            Services.AddSingleton(new HttpClient { BaseAddress = new System.Uri("http://localhost") });
+            Services.AddSingleton<LoginState>();
+            var loginState = Services.GetRequiredService<LoginState>();
+            loginState.SetLogin(false, null);
 
+
+            var navMenuComponent = RenderComponent<NavMenu>();
+
+
+            var kirjauduButton = navMenuComponent.Find("#kirjaudu");
+            kirjauduButton.Click();
+
+
+            await Task.Delay(500);
+
+
+            var loginComponent = RenderComponent<Login>();
+
+
+        }
     }
 }

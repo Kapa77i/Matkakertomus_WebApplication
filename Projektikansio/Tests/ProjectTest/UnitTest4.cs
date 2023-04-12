13fdea5d52a6a37d0e4fb3a3a82161118780083a
Katja Papinniemi <Katja.Papinniemi@edu.savonia.fi>
2023-04-12 19:18:31 +0300

    GroupTrips getti toimii.

--- Projektikansio/Tests/ProjectTest/UnitTest4.cs
+++ Projektikansio/Tests/ProjectTest/UnitTest4.cs
@@ -1,7 +1,10 @@
 ﻿using Bunit;
 using FrontEnd.Pages;
 using FrontEnd.Shared;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using MyApi.Controllers;
+using MyApi.Data;
 using SharedLib;
 using System;
 using System.Collections.Generic;
@@ -28,61 +31,37 @@ namespace ProjectTest
             kirjauduButton.Click();
 
             await Task.Delay(500);
-
-            ////ALustetaan yhteys dbhen
-            //private static async Task<MydbContext> GetDbContext()
-            //{
-            //    var options = new DbContextOptionsBuilder<MydbContext>()
-            //        .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-            //        .Options;
-            //    var databaseContext = new MydbContext(options);
-            //    databaseContext.Database.EnsureCreated();
-            //    //if(await databaseContext.Matkas.CountAsync() > 0)
-            //    //{
-            //    //    databaseContext.Matkas.Add(
-
-            //    //        new Matka()
-            //    //        {
-            //    //            Idmatkaaja = 1,
-            //    //            Alkupvm = DateTime.Parse("2023-03-03"),
-            //    //            Loppupvm = DateTime.Parse("2023-03-03"),
-            //    //            Yksityinen = 1
-            //    //        });
-
-            //    //    await databaseContext.SaveChangesAsync();
-            //    //}
-
-            //    return databaseContext;
-            //}
-
-            ////Tämä facta pittää olla että näkkyyy tuolla test explorelilla <-- Tiedoksi tyhmälle Kapatille
-            //[Fact]
-            //public async Task MatkaController_Get_Returns()
-            //{
-
-            //    //Arrange
-
-            //           //new Matka()
-            //           //{
-            //           //    Idmatkaaja = 1,
-            //           //    Alkupvm = DateTime.Parse("2023-03-03"),
-            //           //    Loppupvm = DateTime.Parse("2023-03-03"),
-            //           //    Yksityinen = 1
-            //           //};
-
-            //    var dbContext = await GetDbContext();
-            //    var matkasController = new MatkasController(dbContext);
-
-            //    //Act 
-
-            //    var result = matkasController.GetMatkas();
-
-            //    //Assert
-            //    result.IsCompletedSuccessfully.ToString();
-            //    result.ToString();
-
-
-            //}
+           
         }
+
+        //Tämä facta pittää olla että näkkyyy tuolla test explorelilla <-- Tiedoksi tyhmälle Kapatille
+        [Fact]
+        public async Task MatkaController_Get_Returns()
+        {
+
+            MydbContext dbContext = new MydbContext();
+            MatkasController matkasController = new MatkasController(dbContext);
+
+            //Act 
+            var result = matkasController.GetMatkas();
+
+            //Assert
+            Assert.NotNull(result);
+
+            //var actionResult = await matkasController.GetMatkas();
+            //Assert.IsType<OkObjectResult>(actionResult.Result);
+            //var resultObject = GetObjectResultContent<List<Matka>>(actionResult);
+
+
+        }
+
+
+        //Vaatii toimiakseen
+        //https://stackoverflow.com/questions/51489111/how-to-unit-test-with-actionresultt/63217643#63217643
+        private static T GetObjectResultContent<T>(ActionResult<T> result)
+        {
+            return (T)((ObjectResult)result.Result).Value;
+        }
+
     }
 }

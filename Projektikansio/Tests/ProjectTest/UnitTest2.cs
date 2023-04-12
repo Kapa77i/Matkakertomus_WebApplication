1528e7427e6367853f76128e9ad0f7709cc1aa77
Sebastian-1995 <81906311+Sebastian-1995@users.noreply.github.com>
2023-04-12 18:28:43 +0300

    Navmenu Loginbutton and registry button test ready

--- Projektikansio/Tests/ProjectTest/UnitTest2.cs
+++ Projektikansio/Tests/ProjectTest/UnitTest2.cs
@@ -34,23 +34,23 @@ namespace ProjectTest
 
             var kirjauduButton = navMenuComponent.Find("#kirjaudu");
             kirjauduButton.Click();
-
-
+            var navigationManager = Services.GetRequiredService<NavigationManager>();
+            Assert.Equal("http://localhost/login", navigationManager.Uri);
             await Task.Delay(500);
 
 
             var loginComponent = RenderComponent<Login>();
 
 
-            var emailInput = loginComponent.Find("#email");
-            var passwordInput = loginComponent.Find("#password");
-            emailInput.Change("sebastian@halonen.com");
-            passwordInput.Change("12345");
+            //var emailInput = loginComponent.Find("#email");
+            //var passwordInput = loginComponent.Find("#password");
+            //emailInput.Change("sebastian@halonen.com");
+            //passwordInput.Change("12345");
 
-            var submitButton = loginComponent.Find("button[type='submit']");
-            submitButton.Click();
+            //var submitButton = loginComponent.Find("button[type='submit']");
+            //submitButton.Click();
 
-            var navigationManager = Services.GetRequiredService<NavigationManager>();
+            
 
             //    Assert.Equal("http://localhost/user/2", navigationManager.Uri);
 
@@ -61,6 +61,28 @@ namespace ProjectTest
         }
 
 
+        [Fact]
+        public async Task TestRegistry()
+        {
+
+            Services.AddSingleton(new HttpClient { BaseAddress = new System.Uri("http://localhost") });
+            Services.AddSingleton<LoginState>();
+            var loginState = Services.GetRequiredService<LoginState>();
+            loginState.SetLogin(false, null);
+
+
+            var navMenuComponent = RenderComponent<NavMenu>();
+
+
+            var RekisteroidyButton = navMenuComponent.Find("#rekisteroidy");
+            RekisteroidyButton.Click();
+            var navigationManager = Services.GetRequiredService<NavigationManager>();
+            Assert.Equal("http://localhost/signup", navigationManager.Uri);
+            await Task.Delay(500);
+
+
+           
+        }
 
     }
 }
